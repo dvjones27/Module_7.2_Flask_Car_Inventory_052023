64ff5a686dcd6aa97dc9150975d938f0b6cd8e61
dvjones27 <102387309+dvjones27@users.noreply.github.com>
2023-05-06 07:29:47 -0400

    Modified API files

--- app/api/routes.py
+++ app/api/routes.py
@@ -4,9 +4,13 @@ from models import db, User, Car, car_schema, cars_schema
 
 api = Blueprint('api',__name__, url_prefix='/api')
 
+@api.route('/getdata')
+def getdata():
+    return {'Hello': 'All'}
+
 @api.route('/cars', methods = ['POST'])
 @token_required
-def create_car(current_user_token):
+def create(current_user_token):
     vin = request.json['vin']
     make = request.json['make']
     model = request.json['model']
@@ -48,7 +52,7 @@ def get_single_car(current_user_token, id):
 
 @api.route('/cars/<id>', methods = ['POST','PUT'])
 @token_required
-def update_car(current_user_token, id):
+def update(current_user_token, id):
     car = Car.query.get(id) 
     car.vin = request.json['vin']
     car.make = request.json['make']
@@ -63,7 +67,7 @@ def update_car(current_user_token, id):
 
 @api.route('/cars/<id>', methods = ['DELETE'])
 @token_required
-def delete_car(current_user_token, id):
+def delete(current_user_token, id):
     car = Car.query.get(id)
     db.session.delete(car)
     db.session.commit()
